84e60bc85a2a189ec700d8f342700f6f0a5594e6
Pavan Yejare <yejare.pavan@gmail.com>
2022-02-23 19:43:23 +0530

    fixed varible type

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2" {
   ami                    = var.ami
   instance_type          = var.instance_type
   availability_zone      = var.zone
-  tags                   = var.tag
+  tags                   = var.tags
   key_name               = var.ssh_key
   vpc_security_group_ids = var.security_group
   subnet_id              = var.subnet
